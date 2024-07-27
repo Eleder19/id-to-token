7ba8ac3395bafc2ff23ddd8e9cf50d2c4f591ca8
Eleder <135975471+Eleder19@users.noreply.github.com>
2024-07-27 18:35:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,19 +12,15 @@ function pause(ms) {
 };
 rl.question('Enter user id: \n', async (data) => {
     try {
-        await new Promise((resolve, reject) => {
             let userId = data.toString().trim();
             let token = btoa(userId);
             if (!userId || userId.length !== 18) {
                 console.log('You have not specified a valid user id!');
                 pause(10000);
-                reject();
             } else {
                 console.log(`First part of the user id's token: ${token}`);
                 pause(10000);
-                resolve();
             };
-        });
     } catch {
         return new Error;
     }
